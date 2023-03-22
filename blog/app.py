10f7bfe70d4cc56312e0ab60fc97934fd83a26b3
aliya <pu_49@>
2023-03-22 17:26:52 +0500

    Last Commit

--- blog/app.py
+++ blog/app.py
@@ -7,6 +7,7 @@ from blog.security import flask_bcrypt
 from blog.views.auth import auth_app, login_manager
 from blog.views.users import users_app
 from blog.views.articles import articles_app
+from blog.views.authors import authors_app
 from blog.models.database import db
 
 app = Flask(__name__)
@@ -28,6 +29,7 @@ flask_bcrypt.init_app(app)
 app.register_blueprint(users_app, url_prefix='/users')
 app.register_blueprint(articles_app, url_prefix='/articles')
 app.register_blueprint(auth_app, url_prefix='/auth')
+app.register_blueprint(authors_app, url_prefix="/authors")
 login_manager.init_app(app)
 
 
